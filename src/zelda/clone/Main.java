5eedb4d8a8d7a1e9336c052b9d8975944e855cd5
Diego Santana <diego_4palco@hotmail.com>
2019-10-01 11:30:08 -0300

    Modo looping, testando com mensagem no console

--- src/zelda/clone/Main.java
+++ src/zelda/clone/Main.java
@@ -1,26 +1,46 @@
 package zelda.clone;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
 
 public class Main extends Canvas implements Runnable {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private static final int WHIDTH = 1366;
 	private static final int HEIGHT = 738;
+	
+	private Boolean isRunning = true;
+	private Thread thread;
 	
 	public Main() {
 		this.setPreferredSize(new Dimension(WHIDTH, HEIGHT));
 		initFrame();
 	}
 	
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		isRunning = false;
+	}
+	
 	public static void main(String [] args) {
 		Main game = new Main();
+		game.start();
 
 	}
 	
@@ -34,12 +54,21 @@ public class Main extends Canvas implements Runnable {
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
 	}
-	
-	
 
 	@Override
 	public void run() {
-		
+	while(isRunning) {
+		tick();
+		render();
+	}
+	}
+	
+	public void render() {
+				
+	}
+
+	private void tick() {
+		System.out.println("Looping sendo executado");
 		
 	}
 }
